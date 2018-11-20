dba6267bb0afd56ff3923f6385fe7c0c6ee5050e
Sergey Ninua <sergeyn@gmail.com>
2018-11-20 11:27:46 +0300

    Distro/release selection

--- run.sh
+++ run.sh
@@ -2,9 +2,16 @@
 set -e
 
 DIR="$(dirname "$(readlink -f "$0")")"
-
 chmod 777 "${DIR}/workdir"
-docker build --tag ubuntu:test "${DIR}"
+
+export DISTRO="${DISTRO:-ubuntu}"
+export RELEASE="${RELEASE:-xenial}"
+
+docker build \
+    --build-arg "DISTRO=$DISTRO" \
+    --build-arg "RELEASE=$RELEASE" \
+    --tag "debuild:${DISTRO}-${RELEASE}" "${DIR}"
+
 docker run -it --rm \
     --mount "type=bind,source=${DIR}/workdir,target=/home/builduser/workdir" \
-    ubuntu:test
+    "debuild:${DISTRO}-${RELEASE}"
